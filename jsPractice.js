f2d07cf97ea158eb8124b91b0d83f5f29b0fea83
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-06-08 17:38:15 -0400

    Reviewing DOM navigation in JS

--- jsPractice.js
+++ jsPractice.js
@@ -37,3 +37,41 @@ BgElement.style.backgroundColor = '#201F2E';
 let headingElement =
     document.querySelector('h1');
 headingElement.style.fontFamily = 'Roboto'
+
+/*
+First, create a variable called first and set it to the first children of the document body.
+Then, set the inner HTML of the first element to 'BROWN BEARS ARE AWESOME!'.
+ Take a moment to note which element has been modified.
+*/
+
+let first = document.body.children[0];
+first.innerHTML = 'BROWN BEARS ARE AWESOME!'
+
+/*
+Use the .parentNode property to access the parent element of the variable first 
+and modify its .style.backgroundColor to 'beige'.
+Take a moment to notice the change in the web page.
+*/
+
+let first = document.body.children[0];
+first.innerHTML = 'BROWN BEARS ARE AWESOME!'
+
+first.parentNode.style.backgroundColor = 'beige';
+
+/*
+Create a <li> element using the .createElement() method and save it in a
+variable called newAttraction.
+
+On the following line, assign the newAttraction element an id of 'vespa'.
+
+Next, assign the newAttraction element the text 'Rent a Vespa' as its inner HTML.
+
+Lastly, append the newAttraction element to the list of top attractions with the 
+ID of italy-attractions.
+*/
+
+let newAttraction = document.createElement('li');
+newAttraction.id = 'vespa';
+newAttraction.innerHTML = 'Rent a Vespa';
+
+document.getElementById('italy-attractions').appendChild(newAttraction);
