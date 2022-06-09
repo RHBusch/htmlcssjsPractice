b42e5af989129766b4289afb48c7ace1f8a4a483
RHBusch <91342001+RHBusch@users.noreply.github.com>
2022-06-09 18:21:39 -0400

    Successfully used JS to implement interactivity in a game for CodeCademy

--- jsPractice.js
+++ jsPractice.js
@@ -75,3 +75,161 @@ newAttraction.id = 'vespa';
 newAttraction.innerHTML = 'Rent a Vespa';
 
 document.getElementById('italy-attractions').appendChild(newAttraction);
+
+/*
+
+First, save the element with the ID of vespa as a variable named elementToRemove.
+
+The elementToRemove element is a child of the list of top attractions with the
+ ID of italy-attractions.
+
+Remove the elementToRemove element from its parent.
+
+*/
+
+let elementToRemove = document.getElementById('vespa');
+document.getElementById('italy-attractions').removeChild(elementToRemove);
+
+
+/*
+Inside the turnButtonRed() function, add the code to modify 
+the button stored in the element variable as follows:
+
+Assign the .style.backgroundColor to 'red'
+Assign the .style.color to 'white'
+Modify the .innerHTML to 'Red Button'
+
+Below our turnButtonRed() function definition, 
+trigger the turnButtonRed() function when the element detects a click event.
+
+*/
+
+let element = document.querySelector('button');
+
+element.onclick = function turnButtonRed() {
+    element.style.backgroundColor = 'red';
+    element.style.color = 'white';
+    element.innerHTML = 'Red Button';
+}
+
+/*
+
+
+Creating JS script for a game on CodeCademy
+
+
+*/
+
+
+let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg';
+let closedDoorPath = 'https://content.codecademy.com/projects/chore-door/images/closed_door.svg';
+
+
+let doorImage1 = document.getElementById('door1');
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
+
+let startButton = document.getElementById('start');
+
+
+
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+let currentlyPlaying = true;
+
+const isClicked = (door) => {
+    if (door.src === closedDoorPath) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const isBot = (door) => {
+    if (door.src === botDoorPath) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const gameOver = (status) => {
+    if (status === 'win') {
+        startButton.innerHTML = 'You win! Play again?'
+    } else {
+        startButton.innerHTML = 'Game over! Play again?'
+    }
+    currentlyPlaying = false;
+}
+
+const playDoor = (door) => {
+    numClosedDoors--;
+    if (numClosedDoors === 0) {
+        gameOver('win')
+    }
+    else if (isBot(door)) {
+        gameOver()
+    }
+}
+
+const randomChoreDoorGenerator = () => {
+    let choreDoor = Math.floor(Math.random * numClosedDoors);
+    if (choreDoor === 0) {
+        openDoor1 = botDoorPath
+        openDoor2 = beachDoorPath
+        openDoor3 = spaceDoorPath
+    } else if (choreDoor === 1) {
+        openDoor1 = beachDoorPath
+        openDoor2 = botDoorPath
+        openDoor3 = spaceDoorPath
+    }
+    else if (choreDoor === 2) {
+        openDoor1 = beachDoorPath
+        openDoor2 = spaceDoorPath
+        openDoor3 = botDoorPath
+    }
+}
+
+doorImage1.onclick = () => {
+    if (currentlyPlaying && isClicked(doorImage1)) {
+        doorImage1.src = openDoor1;
+        playDoor(doorImage1);
+    }
+}
+
+doorImage2.onclick = () => {
+    if (currentlyPlaying && isClicked(doorImage2)) {
+        doorImage2.src = openDoor2;
+        playDoor(doorImage2);
+    }
+}
+
+doorImage3.onclick = () => {
+    if (currentlyPlaying && isClicked(doorImage3)) {
+        doorImage3.src = openDoor3;
+        playDoor(doorImage3);
+    }
+}
+
+startButton.onclick = () => {
+    if (currentlyPlaying === false) {
+        startRound();
+    }
+}
+
+const startRound = () => {
+    doorImage1.src = closedDoorPath;
+    doorImage2.src = closedDoorPath;
+    doorImage3.src = closedDoorPath;
+    numClosedDoors = 3;
+    currentlyPlaying = true;
+    startButton.innerHTML = 'Good Luck!';
+    randomChoreDoorGenerator()
+}
+
+startRound();
